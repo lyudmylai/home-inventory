55a5b3d2ad9c9f014fb9ad8e6e78435a83e3651c
lyudmylai <lyudmyla@mobileappsprogrammer.com>
2018-01-13 15:31:09 -0800

    Configured custom table view cell to display title of the recognized object.

---
+++ HomeInventory/HomeInventory/JETRecognizedItemTableViewCell.h
@@ -0,0 +1,13 @@
+//
+//  JETRecognizedItemTableViewCell.h
+//  HomeInventory
+//
+//  Created by Lyudmyla Ivanova on 1/13/18.
+//  Copyright © 2018 Lyudmyla Ivanova. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface JETRecognizedItemTableViewCell : UITableViewCell
+
+@end
---
+++ HomeInventory/HomeInventory/JETRecognizedItemTableViewCell.m
@@ -0,0 +1,24 @@
+//
+//  JETRecognizedItemTableViewCell.m
+//  HomeInventory
+//
+//  Created by Lyudmyla Ivanova on 1/13/18.
+//  Copyright © 2018 Lyudmyla Ivanova. All rights reserved.
+//
+
+#import "JETRecognizedItemTableViewCell.h"
+
+@implementation JETRecognizedItemTableViewCell
+
+- (void)awakeFromNib {
+    [super awakeFromNib];
+    // Initialization code
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated {
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
